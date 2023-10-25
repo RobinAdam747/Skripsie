f7d09f7572d06ee2581f2a6018f67bc75db766ac
RobinAdam747 <adam.sendzul@gmail.com>
2023-10-25 22:12:34 +0200

    Code cleanup

--- AR_RFID_Annotation_System/Assets/Scripts/ImageTracking.cs
+++ AR_RFID_Annotation_System/Assets/Scripts/ImageTracking.cs
@@ -75,16 +75,4 @@ public class ImageTracking : MonoBehaviour
             }
         }
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
